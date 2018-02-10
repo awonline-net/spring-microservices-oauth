6dc7b1ac8cb64938df9212196b75ec3676f8c476
awonline <awonline.net@gmail.com>
2018-02-10 12:58:41 +0100

    Secured specific services: they act as Resource Server

---
+++ comment-service/src/main/java/net/awonline/microservices/oauth/commentservice/config/ResourceServerConfiguration.java
@@ -0,0 +1,26 @@
+package net.awonline.microservices.oauth.commentservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+        http
+        	.anonymous().disable()
+	        .authorizeRequests().anyRequest().authenticated();
+     // @formatter:on
+	}
+
+	// TODO configure resourceId
+	// @Override
+	// public void configure(ResourceServerSecurityConfigurer resources) throws
+	// super.configure(resources);
+	// }
+}
